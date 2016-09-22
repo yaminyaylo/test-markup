10071e716f82e6e4fa07d30ae416c1504af1be67
Yaroslav Minyaylo <changer.slava@gmail.com>
2016-09-22 09:01:45 +0300

    Added form submit notification

--- js/agency.js
+++ js/agency.js
@@ -5,29 +5,44 @@
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
+      var $anchor = $(this);
+
+      $('html, body').stop().animate({
+          scrollTop: ($($anchor.attr('href')).offset().top - 50)
+      }, 1250, 'easeInOutExpo');
+
+      event.preventDefault();
     });
 
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
+      target: '.navbar-fixed-top',
+      offset: 51
     });
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function(){ 
-        $('.navbar-toggle:visible').click();
+      $('.navbar-toggle:visible').click();
     });
 
     // Offset for Main Navigation
     $('#mainNav').affix({
-        offset: {
-            top: 100
-        }
-    })
+      offset: {
+        top: 100 
+      }
+    });
+
+    // Contact Form Submit
+    $('.form-contact-container button[type=submit]').on('click', function(event) {
+      event.preventDefault();
+      $('.form-contact-container').hide();
+      $('.form-contact-container-on-success').show();
+    });
+
+    $('.form-contact-container-on-success button[type=submit]').on('click', function(event) {
+      event.preventDefault();
+      $('.form-contact-container-on-success').hide();
+      $('.form-contact-container').show();
+    });
 
 })(jQuery); // End of use strict
